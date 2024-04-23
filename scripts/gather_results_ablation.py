adea9337453e15324c85cc05c0a8ba4768166057
Orion Weller <wellerorion@gmail.com>
2024-04-23 09:23:01 -0400

    add scripts for making figures/plots

---
+++ scripts/gather_results_ablation.py
@@ -0,0 +1,124 @@
+import pandas as pd
+import json
+import os
+import glob
+import argparse
+import tqdm
+from collections import defaultdict
+
+from gather_results import model_order, MAP_MODEL_ORDER
+
+
+GRADIENT_STR = """\midpointgradientcell{VALUE}{MIN}{MAX}{0}{neg}{pos}{\opacity}{0}"""
+
+
+def gather_results(args, dataset_in_table=["RobustInstructionRetrieval", "NewsInstructionRetrieval", "CoreInstructionRetrieval"]):
+    # go through all in `results` and aggregate them together
+    # we care only about the pairwise and rankwise scores, as well as map@1000 and ndcg@5 scores of the original and changed
+
+    all_data = []
+    for file in tqdm.tqdm(glob.glob(os.path.join(args.results_dir, "*", "*.json"))):
+        dataset_name = file.split("/")[-1].replace(".json", "")
+        model_name = file.split("/")[-2]
+        with open(file, "r") as f:
+            data = json.load(f)["test"] # all on test set
+
+            # map@1000 and ndcg@5
+            map1000 = data["individual"]["base"]["map_at_1000"]
+            ndcg5 = data["individual"]["base"]["ndcg_at_5"]
+
+            # map@1000 and ndcg@5
+            map1000_full = data["individual"]["original"]["map_at_1000"]
+            ndcg5_full = data["individual"]["original"]["ndcg_at_5"]
+
+            # map@1000 and ndcg@5 of the short_query
+            map1000_short = data["length_ablation"]["short_instructions"][0]["map_at_1000"]
+            ndcg5_short = data["length_ablation"]["short_instructions"][0]["ndcg_at_5"]
+
+            # map@100 and ndcg@5 of the keywords
+            map1000_keywords = data["length_ablation"]["keywords"][0]["map_at_1000"]
+            ndcg5_keywords = data["length_ablation"]["keywords"][0]["ndcg_at_5"]
+
+            # diffs
+            short_diff_map = map1000_short - map1000
+            short_diff_ndcg = ndcg5_short - ndcg5
+
+            keywords_diff_map = map1000_keywords - map1000
+            keywords_diff_ndcg = ndcg5_keywords - ndcg5
+
+            full_diff_map = map1000_full - map1000
+            full_diff_ndcg = ndcg5_full - ndcg5
+
+
+            # add to the list
+            all_data.append({
+                "dataset": dataset_name,
+                "model": model_name,
+                "short_query_score": short_diff_map if "news" not in dataset_name.lower() else short_diff_ndcg,
+                "keywords_score": keywords_diff_map if "news" not in dataset_name.lower() else keywords_diff_ndcg,
+                "full_score": full_diff_map if "news" not in dataset_name.lower() else full_diff_ndcg,
+                "main_score": map1000 if "news" not in dataset_name.lower() else ndcg5
+            })
+
+    keep_columns = ["keywords_score", "short_query_score", "full_score"]
+
+    # create a dataframe
+    df = pd.DataFrame(all_data)
+    # sort by map and then rankwise
+    df = df.sort_values(by=keep_columns, ascending=[False, False, False])
+
+    # lets turn this into a latex figure
+    # aggregate by dataset, and grab only the map (for Robust and Core) or (nDCG) of the original, plus the pointwise and rankwise scores
+    df = df[df["dataset"].isin(dataset_in_table)]
+    df = df.groupby(["dataset", "model"]).agg({"full_score": "first", "keywords_score": "first", "short_query_score": "first"}).reset_index()
+    # for every metric, multiply by 100 and round and format to nearest tenth
+    for col in keep_columns:
+        df[col] = (df[col] * 100).round(1).astype(str)
+
+    # order by model
+    df["model"] = df["model"].map(MAP_MODEL_ORDER)
+    # keep only the ones that are not nan, e.g. not in the model
+    df = df[df.model.notna()]
+    df["model"] = pd.Categorical(df["model"], model_order.keys())
+    df = df.sort_values(by=["model"])
+    df.to_csv(os.path.join(args.results_dir, "ablation_results.csv"), index=False)
+    # First, pivot your DataFrame to get 'model' as index and have a multi-level column with 'dataset' and the scores
+    pivoted_df = df.pivot(index='model', columns='dataset')
+    # Flatten the MultiIndex in columns, concatenating level values
+    pivoted_df.columns = [' '.join(col).strip() for col in pivoted_df.columns.values]
+    # Since you seem to wish for a specific order and naming of columns, you can explicitly reindex/organize
+    # This step might need to be adjusted based on exact desired format, especially if you have dynamic datasets
+    # The list of new column names should be formed based on the datasets and scores present in your original dataframe
+    new_column_order = [
+        'keywords_score RobustInstructionRetrieval', 'short_query_score RobustInstructionRetrieval','full_score RobustInstructionRetrieval', 
+        'keywords_score NewsInstructionRetrieval', 'short_query_score NewsInstructionRetrieval','full_score NewsInstructionRetrieval',
+        'keywords_score CoreInstructionRetrieval', 'short_query_score CoreInstructionRetrieval', 'full_score CoreInstructionRetrieval',
+    ]
+    pivoted_and_ordered_df = pivoted_df.reindex(columns=new_column_order).reset_index()
+    # Now, your DataFrame is in the desired format. If you want to rename the columns for appearance,
+    # you can manually set them as follows (This step is optional and should be customized based on your needs)
+
+    # for each value, if "-" not in it, add "+"
+    x_format = lambda x: str(x) if "-" in str(x) else "+"+str(x)
+    for i, col in enumerate(pivoted_and_ordered_df.columns):
+        if "model" not in col:
+
+            pivoted_and_ordered_df[col] = pivoted_and_ordered_df[col].apply(lambda x: x_format(x))
+
+
+    # Print or return your rearranged DataFrame
+    print(pivoted_and_ordered_df)
+    # add a column at the beginning that is empty
+    pivoted_and_ordered_df.insert(0, "empty", "")
+    
+    pivoted_and_ordered_df.to_latex(os.path.join(args.results_dir, "ablation_results.tex"), index=False)
+    print(f"Saved to {os.path.join(args.results_dir, 'ablation_results.tex')}")
+    print(f"Saved to {os.path.join(args.results_dir, 'ablation_results.csv')}")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--results_dir", default="results", type=str)
+    args = parser.parse_args()
+    gather_results(args)
+            
---
+++ scripts/make_ablation_plots.py
@@ -0,0 +1,98 @@
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pandas as pd
+import argparse
+
+
+
+model2category = {
+    # non instruction tuned
+    "BM25": "No Instruction Training",
+    "E5-base-v2": "No Instruction Training",
+    "E5-large-v2": "No Instruction Training",
+    "Contriever": "No Instruction Training",
+    "MonoBERT": "No Instruction Training",
+    "MonoT5-base": "No Instruction Training",
+    "MonoT5-3B": "No Instruction Training",
+    "Cohere v3 English": "Instruct-Tuned LLMs and APIs",
+    "OpenAI v3 Large": "Instruct-Tuned LLMs and APIs",
+    # now these are instruction-tuned in some sense
+    "E5-mistral": "Uses Instructions in Training",
+    "TART-Contriever": "Uses Instructions in Training",
+    "INSTRUCTOR-base": "Uses Instructions in Training",
+    "INSTRUCTOR-xl": "Uses Instructions in Training",
+    "E5-mistral": "Uses Instructions in Training",
+    "BGE-base": "Uses Instructions in Training",
+    "BGE-large": "Uses Instructions in Training",
+    "TART-FLAN-T5-xl": "Uses Instructions in Training",
+    "GritLM-7B": "Uses Instructions in Training",
+    "FLAN-T5-base": "Instruct-Tuned LLMs and APIs",
+    "FLAN-T5-large": "Instruct-Tuned LLMs and APIs",
+    # LLMs/large rerankers
+    "Llama-2-7B": "Instruct-Tuned LLMs and APIs",
+    "Llama-2-7B-chat": "Instruct-Tuned LLMs and APIs",
+    "Mistral-7B-instruct": "Instruct-Tuned LLMs and APIs",
+    "FollowIR-7B": "Instruct-Tuned LLMs and APIs",
+
+}
+
+def make_ablation_plot(args):
+    df = pd.read_csv(args.file, header=0, index_col=None)
+    df = df[df.dataset.apply(lambda x: "robust" in x.lower())]
+    df["category"] = df.model.apply(lambda x: model2category[x])
+
+    # change TART models to just be TART-FLAN and TART-dual
+    df["model"] = df["model"].apply(lambda x: x.replace("TART-Contriever", "TART-dual").replace("TART-FLAN-T5-xl", "TART-FLAN"))
+    # make INSTRUCTOR-base to just be INSTRUCTOR-b and INSTRUCTOR-xl to INSTRUCTOR-x
+    df["model"] = df["model"].apply(lambda x: x.replace("INSTRUCTOR-base", "INSTRUCTOR-b"))#.replace("INSTRUCTOR-xl", "INSTRUCTOR-x"))
+
+    # Melt the DataFrame for grouped barplot preparation
+    df_melted = df.melt(id_vars=["dataset", "model", "category"], 
+                        value_vars=["keywords_score", "short_query_score", "full_score"], 
+                        var_name="score_type", value_name="score")
+
+    categories = ["No Instruction Training", "Uses Instructions in Training", "Instruct-Tuned LLMs and APIs"]
+    # Set figsize width to maximize page width utilization, and reduce height per subplot
+    fig, axes = plt.subplots(len(categories), 1, figsize=(12, 9), squeeze=False)
+    
+    for i, category in enumerate(categories):
+        cur_df = df_melted[df_melted['category'] == category]
+        # sort by score
+        cur_df = cur_df.sort_values(by="score", ascending=False)
+        ax = sns.barplot(data=df_melted[df_melted['category'] == category], x="model", y="score", 
+                         hue="score_type", ax=axes[i][0], palette=sns.color_palette("ch:s=.25,rot=-.25", as_cmap=False), dodge=True)
+        ax.set_title(category, fontsize=16)
+        # no x-axis subtitle
+        ax.set_xlabel('')
+        ax.tick_params(axis='y', labelsize=13)
+        # set x-axis labels to be labelsize 13
+        ax.set_xticklabels(ax.get_xticklabels(), rotation=0, ha="center", fontsize=12)
+        # This line can be adjusted or commented out if unnecessary
+        ax.set_ylabel('Δ p-MRR compared to using the query only', fontsize=15, labelpad=15)
+        if i != 1:  # Optionally, clear y-labels for non-first subplots
+            ax.set_ylabel('')
+
+    # Placing the legend at the bottom of the figure
+    handles, labels = ax.get_legend_handles_labels()
+    # add some spacing before the legend, and make it a little to the right
+    fig.legend(handles, ["Keywords", "Short Instruction", "Full Instruction"], loc='lower center', ncol=3, fontsize=13, title="Instruction Setting", title_fontsize=15, bbox_to_anchor=(0.55, 0))
+
+    # Removing legends from individual subplots to use the central legend
+    for ax_row in axes:
+        ax_row[0].get_legend().remove()
+    
+    # Adjust the rect to prevent overlap with the bottom legeng
+    plt.tight_layout(rect=[0, 0.1, 1, 1])
+
+    # Save the plot with adjusted dimensions and formatting
+    plt.savefig(args.file.replace(".csv", "_scientific.png"), dpi=300)
+    plt.savefig(args.file.replace(".csv", "_scientific.pdf"), dpi=300)
+    print(f"Saved to {args.file.replace('.csv', '_scientific.png')} and {args.file.replace('.csv', '_scientific.pdf')}")
+    plt.close()
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--file", default="results/ablation_results.csv", type=str)
+    args = parser.parse_args()
+    make_ablation_plot(args)
+    # python scripts/make_ablation_plots.py --file results/ablation_results.csv
